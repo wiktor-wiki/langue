27f2226fe890f730db27a9ae9e87160b1fe6a411
Daniels Kursits <daniels.kursits@gmail.com>
2019-04-16 22:33:03 +0300

    Implement a more robust version

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# langue
-Minimalist js language highlighter
+<h1 align='center'>Langue</h1>
+
+Langue is a minimalistic and composable language highlighter for JS, that I built for fun.
+It's currently used in my [Wiktor notebook engine](https://wiktor-wiki.github.io/).
+
+## Usage
+
+### Import base
+
+```html
+<script src="//gitcdn.link/cdn/evolbug/langue/master/langue.min.js"></script>
+```
+
+### Import languages
+
+```html
+<script src="//gitcdn.link/cdn/evolbug/langue/master/language/html.js"></script>
+<script src="//gitcdn.link/cdn/evolbug/langue/master/language/lua.js"></script>
+```
+
+### Import basic theme (optional)
+
+```html
+<link
+    rel="stylesheet"
+    href="//gitcdn.link/cdn/evolbug/langue/master/langue.css"
+/>
+```
+
+## Basic architectural idea
+
+### Simplicity
+
+-   Only a few common syntax groups:
+    -   Keyword
+    -   Special
+    -   Punctuation
+    -   Comment
+    -   String
+-   Regex based rules
+-   String and comment matchers are auto-built from given start/end tokens
+
+### Composable architecture:
+
+-   Ability to inject definitions within definitions (JS/CSS in HTML)
+-   Best-effort matching - ignore parts that don't match and continue
+-   Ability to build your definitions progressively
--- langue.js
+++ langue.js
@@ -1,142 +1,325 @@
-"use strict";
+/**
+ * Langue highlighter engine
+ *
+ * @author      Daniels Kursits <https://github.com/evolbug>
+ * @copyright   Copyright (c) 2018 Daniels Kursits <https://github.com/evolbug/langue>
+ * @license     http://opensource.org/licenses/MIT MIT License
+ */
 
-function findall(regex_pattern, string_, typename) {
-   var output_list = [];
+langue = (function() {
+    /**
+     * Escape a regex chunk
+     *
+     * @param {string} unsafe
+     * @return {string}
+     */
+    function regEscape(unsafe) {
+        return unsafe.replace(
+            new RegExp("[.*+?^${}()|[\\]\\\\]", "gm"),
+            "\\$&"
+        );
+    }
 
-   while (true) {
-      var a_match = regex_pattern.exec(string_);
-      if (a_match) {
-         output_list.push([
-            typename,
-            a_match[1],
-            a_match.index + (a_match[0].length - a_match[1].length),
-         ]);
-      } else {
-         break;
-      }
-   }
+    /**
+     * Escape a html chunk
+     *
+     * @param {string} unsafe
+     * @return {string}
+     */
+    function htmlEscape(unsafe) {
+        return unsafe
+            .replace(new RegExp("&", "gm"), "&amp;")
+            .replace(new RegExp("<", "gm"), "&lt;")
+            .replace(new RegExp(">", "gm"), "&gt;")
+            .replace(new RegExp('"', "gm"), "&quot;")
+            .replace(new RegExp("'", "gm"), "&#039;");
+    }
 
-   return output_list;
-}
-
-function process(source, tokens, i = 0, last = 0) {
-   return tokens[i]
-      ? source.slice(last, tokens[i][2]) +
-           "<span class='" +
-           tokens[i][0] +
-           "'>" +
-           htmlEscape(tokens[i][1]) +
-           "</span>" +
-           process(source, tokens, i + 1, tokens[i][2] + tokens[i][1].length)
-      : "";
-}
-
-function compose(source, tokens) {
-   for (var typea = 3; typea > 0; typea--) {
-      for (var typeb = typea - 1; typeb >= 0; typeb--) {
-         for (var itema = 0; tokens[typea][itema]; itema++) {
-            for (var itemb = 0; tokens[typeb][itemb]; itemb++) {
-               if (
-                  !(
-                     tokens[typea][itema][2] > tokens[typeb][itemb][2] ||
-                     tokens[typea][itema][2] + tokens[typea][itema][1].length <
-                        tokens[typeb][itemb][2] + tokens[typeb][itemb][1].length
-                  )
-               ) {
-                  tokens[typeb][itemb] = ["", "", 0];
-               }
+    /**
+     * Match a given pattern, tag it with a classname
+     *
+     * @param {string} tag
+     * @param {string} ex
+     * @return {{class:string, value:string, index:number}}
+     */
+    function matcher(tag, ex) {
+        let lookahead = new RegExp("(" + ex + ")");
+        return (code, lastIndex = 0, flags = "ym") => {
+            if (flags) {
+                // Real match
+                let real = new RegExp("(\\s*" + ex + "\\s*)", flags);
+                real.lastIndex = lastIndex;
+                let match = real.exec(code);
+                return match
+                    ? {
+                          class: tag,
+                          value: match[1],
+                          index: match.index,
+                      }
+                    : false;
+            } else {
+                // Lookahead match
+                let match = lookahead.exec(code.substr(lastIndex));
+                return match
+                    ? {
+                          class: tag,
+                          value: match[1],
+                          index: match.index + lastIndex,
+                      }
+                    : false;
             }
-         }
-      }
-   }
-   return process(
-      source.innerText,
-      []
-         .concat(...tokens)
-         .filter(e => e[0])
-         .sort((a, b) => (a[2] >= b[2] ? 1 : -1))
-   );
-}
+        };
+    }
 
-function regEscape(unsafe) {
-   return unsafe.replace(new RegExp("[.*+?^${}()|[\\]\\\\]", "g"), "\\$&");
-}
+    /**
+     * Special
+     * @param {RegExp} bits
+     * @return {matcher}
+     */
+    function sw(bits, ...vars) {
+        let ex = bits.reduce(
+            (r, c, i) => `${r}${c}${vars[i] ? vars[i] : ""}`,
+            ""
+        );
+        return matcher("special", ex);
+    }
 
-function htmlEscape(unsafe) {
-   return unsafe
-      .replace(new RegExp("&", "g"), "&amp;")
-      .replace(new RegExp("<", "g"), "&lt;")
-      .replace(new RegExp(">", "g"), "&gt;")
-      .replace(new RegExp('"', "g"), "&quot;")
-      .replace(new RegExp("'", "g"), "&#039;");
-}
+    /**
+     * Punctuation
+     *
+     * @param {RegExp} bits
+     * @return {matcher}
+     */
+    function p(bits, ...vars) {
+        let ex = bits.reduce(
+            (r, c, i) => `${r}${c}${vars[i] ? vars[i] : ""}`,
+            ""
+        );
+        return matcher("punctuation", ex);
+    }
 
-function loadLanguage(language, success) {
-   language =
-      "https://raw.githubusercontent.com/wiktor-wiki/languages/master/" +
-      language.replace("language-", "") +
-      ".json";
+    /**
+     * Keyword
+     *
+     * @param {RegExp} bits
+     * @return {matcher}
+     */
+    function kw(bits, ...vars) {
+        let ex = bits.reduce(
+            (r, c, i) => `${r}${c}${vars[i] ? vars[i] : ""}`,
+            ""
+        );
+        return matcher("keyword", ex);
+    }
 
-   return $.getJSON(language, { _: $.now() }, function(data) {
-      data.keywords = new RegExp(data.keywords, "gm");
+    /**
+     * String
+     *
+     * @param {"start,end|..."} bits
+     * @return {matcher}
+     */
+    function s(bits, ...vars) {
+        let ex = bits.reduce(
+            (r, c, i) => `${r}${c}${vars[i] ? vars[i] : ""}`,
+            ""
+        );
+        return matcher(
+            "string",
+            ex
+                .split("|")
+                .map(e => e.trim())
+                .map(pair => {
+                    pair = pair.split(",").map(e => regEscape(e.trim()));
+                    return `(?:${pair[0]}(?:\\\\\\\\|\\\\${pair[1]}|.|\\s)*?${
+                        pair[1]
+                    })`;
+                })
+                .join("|")
+        );
+    }
 
-      data.punctuation = new RegExp(
-         "(" +
-            data.punctuation
-               .split("")
-               .map(regEscape)
-               .join("|") +
-            ")",
-         "gm"
-      );
+    /**
+     * Comment
+     *
+     * @param {"start,end|..."} bits
+     * @return {matcher}
+     */
 
-      data.comment = new RegExp(
-         "(" +
-            data.comment
-               .map(pair => "(?:" + pair[0] + "(?:.|\\s)*?" + pair[1] + ")")
-               .join("|") +
-            ")",
-         "gm"
-      );
+    function c(bits, ...vars) {
+        let ex = bits.reduce(
+            (r, c, i) => `${r}${c}${vars[i] ? vars[i] : ""}`,
+            ""
+        );
+        return matcher(
+            "comment",
+            ex
+                .split("|")
+                .map(e => e.trim())
+                .map(pair => {
+                    pair = pair.split(",").map(e => regEscape(e.trim()));
+                    return `(?:${pair[0]}(?:.|\\s)*?${pair[1]})`;
+                })
+                .join("|")
+        );
+    }
 
-      data.string = new RegExp(
-         "(" +
-            data.string
-               .map(
-                  pair =>
-                     "(?:" +
-                     regEscape(pair[0]) +
-                     "(?:\\\\\\\\|\\\\" +
-                     regEscape(pair[1]) +
-                     "|.|\\s)*?" +
-                     regEscape(pair[1]) +
-                     ")"
-               )
-               .join("|") +
-            ")",
-         "gm"
-      );
+    /**
+     * Skip these (no class)
+     *
+     * @param {RegExp} bits
+     * @return {matcher}
+     */
+    function skip(bits, ...vars) {
+        let ex = bits.reduce(
+            (r, c, i) => `${r}${c}${vars[i] ? vars[i] : ""}`,
+            ""
+        );
+        return matcher("", ex);
+    }
 
-      success(data);
-   });
-}
+    /**
+     * Parse a chunk of {code} using {pattern} starting from {lastIndex}
+     *
+     * @param {string} code
+     * @param {array|matcher} pattern
+     * @param {number} lastIndex
+     * @return {array}
+     */
+    function parse(code, pattern, lastIndex = 0) {
+        let results = { matches: [], lastIndex: lastIndex };
+        let stuckIndex = lastIndex;
 
-function highlight(code) {
-   if (code.className) {
-      loadLanguage(code.className, function(lang) {
-         var keywords = findall(lang.keywords, code.innerText, "keyword");
-         var punctuation = findall(
-            lang.punctuation,
-            code.innerText,
-            "punctuation"
-         );
-         var comments = findall(lang.comment, code.innerText, "comment");
-         var strings = findall(lang.string, code.innerText, "string");
+        for (let chunk_id = 0; chunk_id < pattern.length; chunk_id++) {
+            let chunk = pattern[chunk_id];
 
-         code.innerHTML = compose(
-            code,
-            [punctuation, keywords, strings, comments]
-         );
-      });
-   }
-}
+            if (Array.isArray(chunk)) {
+                let subset = parse(code, chunk, results.lastIndex);
+
+                while (subset && subset.lastIndex > stuckIndex) {
+                    stuckIndex = subset.lastIndex;
+                    results.matches = results.matches.concat(subset.matches);
+                    results.lastIndex = subset.lastIndex;
+                    subset = parse(code, chunk, results.lastIndex);
+                }
+            } else {
+                let match = chunk(code, results.lastIndex);
+                if (!match) return false;
+
+                results.lastIndex = match.index + match.value.length;
+                results.matches.push(match);
+            }
+        }
+
+        return results;
+    }
+
+    /**
+     * Define a new language
+     *
+     * @param {array[]} definition
+     * @return {function}
+     */
+    function Language(definition) {
+        return function(code) {
+            let match = true;
+            let tokens = [];
+            let lastIndex = 0;
+            let stuckIndex = -1;
+
+            while (match && lastIndex > stuckIndex) {
+                let initial_matches = [];
+                match = false;
+                stuckIndex = lastIndex;
+
+                for (let pattern = 0; pattern < definition.length; pattern++) {
+                    let initial = definition[pattern][0](
+                        code,
+                        lastIndex,
+                        false
+                    );
+
+                    if (initial) {
+                        match = true;
+                        initial_matches.push({
+                            pattern: pattern,
+                            match: initial,
+                        });
+                    }
+                }
+
+                initial_matches = initial_matches.sort(
+                    (a, b) => a.match.index - b.match.index
+                );
+
+                for (let match of initial_matches) {
+                    let result = parse(
+                        code,
+                        definition[match.pattern],
+                        match.match.index
+                    );
+                    if (result) {
+                        lastIndex = result.lastIndex;
+                        tokens = tokens.concat(result.matches);
+                        break;
+                    }
+                }
+            }
+
+            let result = [];
+            let offset = 0;
+            for (let t = 0; t < tokens.length; t++) {
+                result.push(htmlEscape(code.slice(offset, tokens[t].index)));
+                result.push(
+                    `<span class='${tokens[t].class}'>${htmlEscape(
+                        tokens[t].value
+                    )}</span>`
+                );
+                offset = tokens[t].index + tokens[t].value.length;
+            }
+
+            result.push(htmlEscape(code.slice(offset)));
+            return result.join("");
+        };
+    }
+
+    /**
+     * Highlight any string using the given language
+     *
+     * @param {string} code
+     * @param {string} language
+     * @return {string}
+     */
+    function langue(code, language) {
+        if (langue.languages[language]) {
+            return langue.languages[language](code);
+        } else {
+            console.error(`requested language ${language} is not defined`);
+            return code;
+        }
+    }
+
+    /**
+     * Highlight a DOM object by automatically detecting language from classname
+     *
+     * @param {Node} code
+     */
+    langue.auto = function(code) {
+        if (code.className)
+            code.innerHTML = langue(
+                code.innerText,
+                code.className.match(/language-(\w+)/)[1]
+            );
+    };
+
+    /**
+     * Language parser function cache
+     */
+    langue.languages = {};
+
+    /**
+     * Syntax definition array cache
+     */
+    langue.syntax = {};
+
+    return langue;
+})();
